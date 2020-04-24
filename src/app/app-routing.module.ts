54a35aa5f2527810d456d248b9f95ceacaf287e5
faruqneo <faruq_khan@yahoo.com>
2020-04-24 17:20:02 +0530

    fix issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ProductComponent } from './product/product/product.component';
 import { AuthGuard } from './auth.guard';
 
 
 const routes: Routes = [
-  // { path: 'product', component: ProductComponent },
-  // { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent },
   {
     path: '',
     children: [
